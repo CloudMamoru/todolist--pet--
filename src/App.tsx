bb214eaa04e50e4be59700376061b996f2443f08
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2023-11-04 20:16:07 +0300

    Initialized git repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Todolist, TaskType } from './Todolist';
+
+let task1: Array<TaskType> = [
+  { id: 1, title: 'CSS', isDone: true },
+  { id: 2, title: 'JS', isDone: true },
+  { id: 3, title: 'React', isDone: false },
+];
+
+let task2: Array<TaskType> = [
+  { id: 1, title: 'Python', isDone: false },
+  { id: 2, title: 'Django', isDone: true },
+  { id: 3, title: 'DRF', isDone: true },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Todolist title='Music' tasks={task1} />
+      <Todolist title='Rock' tasks={task2} />
     </div>
   );
 }
